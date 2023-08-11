c8e35719d855f850915ed7f73b3850f8a9907471
kirillP1 <kirillpushka624@gmail.com>
2023-08-11 14:55:45 +0300

    paralax-goods-slider

---
+++ js/swiper-slider.js
@@ -0,0 +1,11 @@
+new Swiper('.slider-j', {
+	// direction: 'vertical',
+	// freeMode: true,
+	speed: 2400,
+	parallax: true,
+	spaceBetween: 18,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+})
